a7e86069356b0a916392d6e2aa114c44a0e9d2ac
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2023-08-28 19:44:50 -0600

    Scope/Paralelo

--- prodAx_for.c
+++ prodAx_for.c
@@ -1,16 +1,6 @@
-//--------------------------------------------------------------
-// prodAx_.c
-//--------------------------------------------------------------
-// Calcula el producto de una matriz m x n por un vector tamaño n
-// y mide el tiempo de ejecución.
-//--------------------------------------------------------------
-// Auth.  JJCelada - Universidad del Valle de Guatemala
-// Date   2021-10-06
-// Vers.  1.0
-
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h> // Incluimos la biblioteca OpenMP
 #include <time.h> // Incluimos la biblioteca para medir el tiempo
 
 void prodAx(int m, int n, double * restrict A, double * restrict x,
@@ -49,7 +39,7 @@ int main(int argc, char *argv[]) {
   (void) prodAx(m, n, A, x, b);
   clock_t end_time = clock(); // Medimos el tiempo después de la llamada a la función
 
-  printf("Time taken: %f seconds\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+  
 
   printf("\nb: \n");
   for(j=0; j<n; j++)
@@ -58,7 +48,10 @@ int main(int argc, char *argv[]) {
 
   free(A);free(x);free(b);
 
+  printf("Time taken: %f seconds\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
   return(0);
+  
 }
 
 /* ------------------------
@@ -70,11 +63,12 @@ void prodAx(int m, int n, double * restrict A, double * restrict x,
 
   int i, j;
 
+  #pragma omp parallel for private(j) // Iniciamos un bucle paralelo
   for(i = 0; i < m; i++) {
     b[i] = 0.0;
 
     for(j = 0; j < n; j++) {
       b[i] += A[i*n + j] * x[j];
     }
-  }
+  } // Terminamos el bucle paralelo
 }
